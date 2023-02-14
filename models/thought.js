822f1d9aa0e54d72915b38e2c364d14df71a4c2e
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2023-02-14 14:51:11 -0500

    Created thought.js in models

--- models/thought.js
+++ models/thought.js
@@ -1,1 +1,71 @@
-//
+const {Schema, model, Types} = require('mongoose');
+
+const date = require('moment');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: [1, 'Thought must be at least 1 character long'],
+            maxlength: [280, 'Thought must be less than 280 characters']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => date(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: [true, 'Username is required']
+        
+        },
+
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
+
+const Thought = model('Thought', ThoughtSchema);
+
+
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: [280, 'Reaction must be less than 280 characters']
+        },
+        username: {
+            type: String,
+            required: [true, 'Username is required']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => date(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+module.exports = Thought;
